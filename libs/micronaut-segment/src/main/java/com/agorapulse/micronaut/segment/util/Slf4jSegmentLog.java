f565ef2008db0f84782543e9fba9aab0bffbd7cd
Vladimir Orany <vladimir@orany.cz>
2021-07-22 15:19:28 +0200

    Added Segment logging, blocking for functions

---
+++ libs/micronaut-segment/src/main/java/com/agorapulse/micronaut/segment/util/Slf4jSegmentLog.java
@@ -0,0 +1,61 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020-2021 Agorapulse.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.agorapulse.micronaut.segment.util;
+
+import com.segment.analytics.Analytics;
+import com.segment.analytics.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jSegmentLog implements Log {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Analytics.class);
+
+    @Override
+    public void print(Level level, String format, Object... args) {
+        switch (level) {
+            case VERBOSE:
+                LOGGER.trace(format, args);
+                break;
+            case DEBUG:
+                LOGGER.debug(format, args);
+                break;
+            case ERROR:
+                LOGGER.error(format, args);
+                break;
+        }
+    }
+
+    @Override
+    public void print(Level level, Throwable error, String format, Object... args) {
+        switch (level) {
+            case VERBOSE:
+                LOGGER.trace(format, args);
+                LOGGER.trace("", error);
+                break;
+            case DEBUG:
+                LOGGER.debug(format, args);
+                LOGGER.debug("", error);
+                break;
+            case ERROR:
+                LOGGER.error(format, args);
+                LOGGER.error("", error);
+                break;
+        }
+    }
+}
